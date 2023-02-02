ff9ed5df41aa4603c5fca30dbe1e4ad6c6bafbe9
ABBARAJU VENKATA NAGA SAI JAYANTH <113752293+Jayanthvenkata@users.noreply.github.com>
2023-02-02 09:50:48 +0530

    Add files via upload

---
+++ day1-progarm.py
@@ -0,0 +1,5 @@
+x=int(input("no of times"))
+y=x*3
+z=y+56.19
+a=z-10
+print("the final result",a
---
+++ day1-program.py
@@ -0,0 +1,8 @@
+kumar=int(input("no of kgs given by the kumar"))
+sam=kumar/2
+kumar=kumar/2
+kumar=kumar+(sam/2)
+sam=sam/2
+print("left by cane",kumar)
+print("half have sam",sam)
+print("total cane",kumar+sam)
